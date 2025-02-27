339c5fd88a7b2e6ffb1a07d5918c9d9adf3cea75
Smb@2004 <burungaleswapnil0407@gmail.com>
2025-02-27 22:36:28 +0530

    changes

---
+++ vowelConsonant.java
@@ -0,0 +1,9 @@
+public class vowelConsonant{
+    public static void main(String[] args){
+        char ch='A';
+        while(ch<='Z'){
+            if(ch!='A'&& ch!='E'&& ch!='I'&& ch!='O'&& ch!='U'){
+                System.out.print(ch+" ");
+            }ch++;
+    }
+}
