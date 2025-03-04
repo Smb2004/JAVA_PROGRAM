b913837eb306f1e4824edc04877efc9357f69233
Smb@2004 <burungaleswapnil0407@gmail.com>
2025-03-04 11:41:25 +0530

    changes

---
+++ PrimeNum.java
@@ -0,0 +1,23 @@
+import java.io.*;
+public class PrimeNum {
+    public static void main(String[] args)throws IOException{
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        System.out.println("Enter num:");
+        int num = Integer.parseInt(br.readLine());
+    
+        boolean isPrime=false;
+        for(int i=2;i<Math.sqrt(num);i++){
+
+            if (num%i==0){
+                isPrime=false;
+                break;
+            }
+        }
+        if(isPrime){
+            System.out.println("Number is prime");
+        }else{
+            System.out.println("Number is not prime");
+        }
+    }   
+    
+}
