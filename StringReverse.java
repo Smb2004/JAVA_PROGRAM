a0a5ef969a5062a7f4dd5138321dd71842e29d58
Smb@2004 <burungaleswapnil0407@gmail.com>
2025-03-22 14:46:35 +0530

    changes

---
+++ StringReverse.java
@@ -0,0 +1,13 @@
+import java.io.*;
+public class StringReverse{
+        public static void main(String[]args)throws IOException{
+                BufferedReader br=new BufferedReader(new InputStreamReader(System.in));
+                System.out.println("Enter string:");
+                String str1=br.readLine();
+
+                StringBuffer str2=new StringBuffer(str1);
+
+                str2=str2.reverse();
+                System.out.println(str2);
+        }
+}
