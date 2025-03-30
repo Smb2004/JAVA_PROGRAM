3e7f746d642bdfbdf53e60ae16c875131b4636a0
Smb@2004 <burungaleswapnil0407@gmail.com>
2025-03-31 00:00:17 +0530

    changes

---
+++ InstanceVariable.java
@@ -0,0 +1,11 @@
+public class InstanceVariable {
+    int x=10;
+    int y=20;
+    public static void main(String[] args){
+        InstanceVariable obj=new InstanceVariable();
+        System.out.println(obj.x);
+        System.out.println(obj.y);
+        
+    }
+    
+}
