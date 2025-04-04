9776844ad5926202c68f81407abf545bbce96718
Smb@2004 <burungaleswapnil0407@gmail.com>
2025-04-04 21:49:05 +0530

    changes

---
+++ Pattern11.java
@@ -0,0 +1,18 @@
+import java.util.*;
+public class Pattern11 {
+    public static void main(String[] args){
+        Scanner sc=new Scanner(System.in);
+        System.out.println("Enter row");
+        int row=sc.nextInt();
+        for(int i=1;i<=row;i++){
+            int n=1;
+            for(int sp=1;sp<=row-i;sp++){
+                System.out.print("\t");
+
+            }for(int j=1;j<=i;j++){
+                System.out.print(n+"\t");
+                n++;
+            }System.out.println();
+        }
+    }
+}
