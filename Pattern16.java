70bb72f4b9a727f8e47bda65643cf2426de47736
Smb@2004 <burungaleswapnil0407@gmail.com>
2025-04-09 00:03:27 +0530

    changes

--- Pattern16.java
+++ Pattern16.java
@@ -4,16 +4,16 @@ public class Pattern16{
         Scanner sc=new Scanner(System.in);
         System.out.println("Enter row:");
         int row=sc.nextInt();
-        int n=row;
+        int num=row;
         for(int i=1;i<=row;i++){
             
             for(int sp=1;sp<=i-1;sp++){
                 System.out.print("\t");
 
             }for(int j=1;j<=row+1-i;j++){
-                System.out.print(n+"\t");
+                System.out.print(num+"\t");
                 
-            }n--;
+            }num--;
             System.out.println();
         }
     }
