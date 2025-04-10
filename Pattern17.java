f264a387cd9bf2a297101e07a17121c61db0ff55
Smb@2004 <burungaleswapnil0407@gmail.com>
2025-04-10 09:56:58 +0530

    changes

---
+++ Pattern17.java
@@ -0,0 +1,19 @@
+import java.util.*;
+public class Pattern17{
+    public static void main(String[] args){
+        Scanner sc=new Scanner(System.in);
+        System.out.println("Enter row:");
+        int row=sc.nextInt();
+        
+        for(int i=1;i<=row;i++){
+            int num=1;
+            for(int sp=1;sp<=i-1;sp++){
+                System.out.print("\t");
+
+            }for(int j=1;j<=row+1-i;j++){
+                System.out.print(num+"\t");
+                num++;
+            }System.out.println();
+        }
+    }
+}  
