721e0972a27cebf4a0d3c1547a0d8df5d72a8718
Smb@2004 <burungaleswapnil0407@gmail.com>
2025-04-10 23:14:43 +0530

    changes

---
+++ Pattern19.java
@@ -0,0 +1,20 @@
+import java.util.*;
+public class Pattern19{
+    public static void main(String[] args){
+        Scanner sc=new Scanner(System.in);
+        System.out.println("Enter row:");
+        int row=sc.nextInt();
+        
+        for(int i=1;i<=row;i++){
+            char ch=(char)(row+64);
+            for(int sp=1;sp<=i-1;sp++){
+                System.out.print("\t");
+
+            }for(int j=1;j<=row+1-i;j++){
+                System.out.print(ch+"\t");
+                ch--;
+                
+            }System.out.println();
+        }
+    }
+}  
