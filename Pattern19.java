fbe85d6b60303a427fdcb5ba3a768fae351370bb
Smb@2004 <burungaleswapnil0407@gmail.com>
2025-04-16 22:35:57 +0530

    changes

--- Pattern19.java
+++ Pattern19.java
@@ -6,13 +6,13 @@ public class Pattern19{
         int row=sc.nextInt();
         
         for(int i=1;i<=row;i++){
-            char ch=(char)(row+64);
+            char cha=(char)(row+64);
             for(int sp=1;sp<=i-1;sp++){
                 System.out.print("\t");
 
             }for(int j=1;j<=row+1-i;j++){
-                System.out.print(ch+"\t");
-                ch--;
+                System.out.print(cha+"\t");
+                cha--;
                 
             }System.out.println();
         }
